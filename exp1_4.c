bd1e69f9957c6f999f2cd1f48ab3b5a215de080f
Priyanshu Rawat <priyanshurawat4852@gmail.com>
2025-10-06 20:08:25 +0530

    Create exp1_4.c

---
+++ exp1_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter the number a: ");
+    scanf("%d", &a);
+    printf("enter the number b: ");
+    scanf("%d", &b);
+    printf("the sum is: %d", a + b);
+    return 0;
+}
